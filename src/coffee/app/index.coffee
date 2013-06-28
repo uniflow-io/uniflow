f72d47ed6777e2c5e8b6105cf4a3837dc6d48bc6
Mathieu Ledru <matyo91@gmail.com>
2013-06-28 12:56:23 +0200

    clean

--- src/coffee/app/index.coffee
+++ src/coffee/app/index.coffee
@@ -1,7 +1,6 @@
 define ['jquery','ace/ace','bootstrap'], ($,ace) ->
   input = ace.edit 'input'
   output = ace.edit 'output'
-  console.log 'coco', 'dodo'
 
   input.on 'change', () ->
     output.setValue input.getValue(), -1
--- web/js/app/index.js
+++ web/js/app/index.js
@@ -3,7 +3,6 @@
     var input, output;
     input = ace.edit('input');
     output = ace.edit('output');
-    console.log('coco', 'dodo');
     return input.on('change', function() {
       return output.setValue(input.getValue(), -1);
     });
