ac35c6da198b732846688941dc969b417f7292aa
Mathieu Ledru <matyo91@gmail.com>
2013-09-26 17:43:22 +0200

    php filter

--- web/js/lib/searchreplace/filter/gettersandsetters.js
+++ web/js/lib/searchreplace/filter/gettersandsetters.js
@@ -16,10 +16,8 @@ The main class required to get started with searchreplace
     return function(input) {
       var opts, path, php;
       path = window.location.pathname;
-      console.log(path);
       opts = {
         SERVER: {
-          'couuoc': 'too',
           SCRIPT_FILENAME: path.substring(0, path.length - 1)
         },
         filesystem: new PHP.Adapters.XHRFileSystem()
