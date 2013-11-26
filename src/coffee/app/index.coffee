8fbf66d9f9d91520577b1b4f283f5af81bfac4ae
Mathieu Ledru <matyo91@gmail.com>
2013-11-26 14:09:10 +0100

    index remove searchreplace lib

--- src/coffee/app/index.coffee
+++ src/coffee/app/index.coffee
@@ -3,38 +3,25 @@ define [
   'ace/ace',
   'bootstrap',
   'searchreplace',
-  'searchreplace/graph',
-  'searchreplace/data/string',
-], ($, ace, bootstrap, searchreplace, SRGraph, SRStringData) ->
-  # Load some graph definition into NoFlo
+], ($, ace, bootstrap, searchreplace) ->
   noflo = require 'noflo'
-  noflo.graph.loadFBP "'Hello, World!' -> IN Display(Output)", (graph) ->
-
-    # Make all included component libraries available
-    graph.baseDir = "/searchreplace"
-
-    # Create the live NoFlo network
-    noflo.createNetwork graph, (network) ->
-      console.log "Network created"
 
   input = ace.edit 'input'
   output = ace.edit 'output'
 
-  inputGraph = new SRGraph(new SRStringData(input.getValue()));
-  outputGraph = new SRGraph(new SRStringData(output.getValue()));
-
   update = ->
-    inputGraph.update()
-    output.setValue outputGraph.data.get(), -1
+    noflo.graph.loadFBP "'Hello, World!' -> IN Display(Output)", (graph) ->
+      graph.baseDir = "/searchreplace"
+      graph.addInitial input.getValue(), 'Display', 'in'
+      noflo.createNetwork graph, (network) ->
+        console.log "Network created"
+
+    output.setValue '', -1
 
   $('#filters').on 'change', ->
-    require ['searchreplace/filter/' + $(@).val()], (SRFilter) ->
-      inputGraph.detach(outputGraph)
-      inputGraph.attach(new SRFilter(), outputGraph)
-      update()
 
   $('#filters').trigger('change')
 
   input.on 'change', () ->
-    inputGraph.data.set(input.getValue())
+    input.getValue()
     update()
--- web/js/app/index.js
+++ web/js/app/index.js
@@ -1,31 +1,23 @@
 (function() {
-  define(['jquery', 'ace/ace', 'bootstrap', 'searchreplace', 'searchreplace/graph', 'searchreplace/data/string'], function($, ace, bootstrap, searchreplace, SRGraph, SRStringData) {
-    var input, inputGraph, noflo, output, outputGraph, update;
+  define(['jquery', 'ace/ace', 'bootstrap', 'searchreplace'], function($, ace, bootstrap, searchreplace) {
+    var input, noflo, output, update;
     noflo = require('noflo');
-    noflo.graph.loadFBP("'Hello, World!' -> IN Display(Output)", function(graph) {
-      graph.baseDir = "/searchreplace";
-      return noflo.createNetwork(graph, function(network) {
-        return console.log("Network created");
-      });
-    });
     input = ace.edit('input');
     output = ace.edit('output');
-    inputGraph = new SRGraph(new SRStringData(input.getValue()));
-    outputGraph = new SRGraph(new SRStringData(output.getValue()));
     update = function() {
-      inputGraph.update();
-      return output.setValue(outputGraph.data.get(), -1);
-    };
-    $('#filters').on('change', function() {
-      return require(['searchreplace/filter/' + $(this).val()], function(SRFilter) {
-        inputGraph.detach(outputGraph);
-        inputGraph.attach(new SRFilter(), outputGraph);
-        return update();
+      noflo.graph.loadFBP("'Hello, World!' -> IN Display(Output)", function(graph) {
+        graph.baseDir = "/searchreplace";
+        graph.addInitial(input.getValue(), 'Display', 'in');
+        return noflo.createNetwork(graph, function(network) {
+          return console.log("Network created");
+        });
       });
-    });
+      return output.setValue('', -1);
+    };
+    $('#filters').on('change', function() {});
     $('#filters').trigger('change');
     return input.on('change', function() {
-      inputGraph.data.set(input.getValue());
+      input.getValue();
       return update();
     });
   });
