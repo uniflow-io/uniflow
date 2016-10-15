3cbed1175908dc0e3eaa550bd91d11b3be0d1959
Mathieu Ledru <matyo91@gmail.com>
2016-10-15 21:12:18 +0200

    replace select2 and tagit

--- web/js/app.js
+++ web/js/app.js
@@ -3,8 +3,8 @@ import Vue from 'vue'
 import store from './store.js'
 import router from './router.js'
 
-import './directives/select2.js'
-import './directives/tagit.js'
+import './components/select2.js'
+import './components/tagit.js'
 
 import LayoutSection from './sections/layout/index.js'
 
---
+++ web/js/components/select2.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import $ from 'jquery'
+import 'select2'
+
+Vue.component('select2', {
+    props: ['options', 'value'],
+    template: '<select><slot></slot></select>',
+    mounted: function () {
+        var vm = this;
+        $(this.$el)
+            .val(this.value)
+            .select2({ data: this.options })
+            .on('change', function () {
+                vm.$emit('input', this.value)
+            })
+    },
+    watch: {
+        value: function (value) {
+            $(this.$el).select2('val', value)
+        },
+        options: function (options) {
+            $(this.$el).select2({ data: options })
+        }
+    },
+    destroyed: function () {
+        $(this.$el).off().select2('destroy')
+    }
+});
