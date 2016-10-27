f81b7f0e67dfe80df2f7430b46adeccf5d8bec94
Mathieu Ledru <matyo91@gmail.com>
2016-10-27 21:25:04 +0200

    javascript

--- web/js/uniflow/components.js
+++ web/js/uniflow/components.js
@@ -1,3 +1,4 @@
+import CoreJavascript from './core/javascript/index.js';
 import CoreSFTP from './core/sftp/index.js';
 import CoreText from './core/text/index.js';
 import CoreYAML from './core/yaml/index.js';
@@ -6,6 +7,7 @@ import CoreReplace from './core/replace/index.js';
 import DecleorTrads from './decleor/trads/index.js';
 
 export default {
+    'core-javascript': CoreJavascript,
     'core-sftp': CoreSFTP,
     'core-text': CoreText,
     'core-yaml': CoreYAML,
---
+++ web/js/uniflow/core/javascript/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import template from './template.html!text'
+
+import TextMessage from '../../../messages/text.js'
+
+export default Vue.extend({
+    template: template,
+    props: ['message'],
+    created: function() {
+        this.handle(this.message);
+    },
+    data() {
+        return {
+            content: null
+        }
+    },
+    methods: {
+        handleTypes: function() {
+            return [undefined, TextMessage];
+        },
+        handle: function (message) {
+            if(message instanceof TextMessage) {
+                this.content = message.text;
+            }
+        },
+        onSubmit: function(e) {
+            this.$dispatch('message', new TextMessage(this.content));
+        }
+    }
+});
