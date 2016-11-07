e2bbcdade7f6b313fce111e75286c25513e685d0
Mathieu Ledru <matyo91@gmail.com>
2016-11-07 22:13:23 +0100

    ace

--- web/js/components/ace.js
+++ web/js/components/ace.js
@@ -7,12 +7,11 @@ Vue.component('ace', {
     template: '<div :style="{height: height ? height + \'px\' : \'100%\',width: width ? width + \'px\' : \'100%\'}" ><div />',
     mounted: function () {
         var vm = this;
-        $(this.$el).val(this.value);
         var editor = vm.editor = ace.edit(this.$el);
+        editor.$blockScrolling = Infinity;
         editor.on('change', function() {
-            //vm.value = editor.getValue();
             vm.$emit('input', editor.getValue())
-        })
+        });
 
         var session = editor.getSession();
         //session.setMode('ace/mode/javascript');
@@ -21,7 +20,9 @@ Vue.component('ace', {
     },
     watch: {
         value: function (value) {
-            //this.editor.setValue(value, 1);
+            if(value !== undefined) {
+                this.editor.setValue(value, 1);
+            }
         }
     },
     destroyed: function () {
