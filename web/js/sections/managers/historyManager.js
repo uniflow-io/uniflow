0c4ed75209d590473251611948721bad744338f8
Mathieu Ledru <matyo91@gmail.com>
2016-11-09 16:06:36 +0100

    manager

--- web/js/sections/managers/historyManager.js
+++ web/js/sections/managers/historyManager.js
@@ -4,12 +4,12 @@ export default Vue.extend({
     created: function () {
         this.$store.dispatch('fetchHistory').then(() => {
             if(this.$route.name == 'homeDetail') {
-                this.$store.dispatch('setCurrentHistory', this.$route.params.id);
+                this.changeHistory(this.$route.params.id);
             } else {
                 var keys = Object.keys(this.history);
                 if(keys.length > 0) {
                     var item = this.history[keys[0]];
-                    this.$store.dispatch('setCurrentHistory', item.id);
+                    this.changeHistory(item.id);
                 }
             }
         })
@@ -22,14 +22,56 @@ export default Vue.extend({
     watch: {
         '$route': function(to) {
             if(to.name == 'homeDetail') {
-                this.$store.dispatch('setCurrentHistory', to.params.id);
+                this.changeHistory(to.params.id);
             }
         },
         history: {
             handler: function (val, oldVal) {
-                //console.log('historyManagerWatch', val, oldVal);
+                console.log('historyManagerWatch', val, oldVal);
             },
             deep: true
+        }
+    },
+    methods: {
+        serialiseFlowData: function (data) {
+            data = data.slice(0);
+
+            for(let i = 0; i < data.length; i++) {
+                delete data[i].bus
+            }
+
+            return data;
+        },
+        deserialiseFlowData: function (data) {
+            data = data.slice(0);
+
+            for(let i = 0; i < data.length; i++) {
+                data[i].bus = new Vue();
+            }
+
+            return data;
+        },
+        changeHistory: function (current) {
+            Promise.resolve()
+                .then(() => {
+                    var currentHistory = this.$store.getters.currentHistory;
+                    if(currentHistory) {
+                        currentHistory.data = this.serialiseFlowData(this.$store.state.flow.stack);
+                        return this.$store.commit('updateHistory', currentHistory);
+                    }
+                })
+                .then(() => {
+                    this.$store.dispatch('setCurrentHistory', current);
+
+                    return this.$store.dispatch('setFlow', []);
+                })
+                .then(() => {
+                    var currentHistory = this.$store.getters.currentHistory;
+                    if(currentHistory && currentHistory.data) {
+                        return this.$store.dispatch('setFlow', this.deserialiseFlowData(currentHistory.data));
+                    }
+                })
+            ;
         }
     }
 });
