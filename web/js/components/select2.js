f82676da0d5121eaa74bb055185362d05209dd19
Mathieu Ledru <matyo91@gmail.com>
2016-11-17 21:42:09 +0100

    silence-select2

--- web/js/components/select2.js
+++ web/js/components/select2.js
@@ -7,19 +7,25 @@ Vue.component('select2', {
     template: '<select><slot></slot></select>',
     mounted: function () {
         var vm = this;
+        vm.silence = false;
         $(this.$el)
             .val(this.value)
             .select2({ data: this.options })
             .on('change', function () {
+                if(vm.silence) return;
+
                 vm.$emit('input', this.value)
             })
     },
     watch: {
         value: function (value) {
+            var vm = this;
             //$(this.$el).select2('val', value)
 
+            vm.silence = true;
             $(this.$el).val(value);
             $(this.$el).trigger('change.select2');
+            vm.silence = false;
         },
         options: function (options) {
             $(this.$el).select2({ data: options })
