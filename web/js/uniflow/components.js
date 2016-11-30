d42b01d6726ca9e4f703f9cdddae018524bcbc71
Mathieu Ledru <matyo91@gmail.com>
2016-11-30 22:51:27 +0100

    select 😊

--- web/js/uniflow/components.js
+++ web/js/uniflow/components.js
@@ -2,10 +2,12 @@ import CoreJavascript from './core/javascript/index.js';
 import CoreAssets from './core/assets/index.js';
 import CoreText from './core/text/index.js';
 import UICheckBoxes from './ui/checkboxes/index.js';
+import UISelect from './ui/select/index.js';
 
 export default {
     'core-javascript': CoreJavascript,
     'core-assets': CoreAssets,
     'core-text': CoreText,
     'ui-checkboxes': UICheckBoxes,
+    'ui-select': UISelect,
 };
---
+++ web/js/uniflow/ui/select/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import template from './template.html!text'
+
+export default Vue.extend({
+    props: ['bus'],
+    template: template,
+    data() {
+        return {
+            variable: null,
+            select: null
+        }
+    },
+    created: function () {
+        this.bus.$on('reset', this.deserialise);
+        this.bus.$on('execute', this.onExecute);
+    },
+    destroyed: function () {
+        this.bus.$off('reset', this.deserialise);
+        this.bus.$off('execute', this.onExecute);
+    },
+    watch: {
+        variable: function () {
+            this.onUpdate();
+        },
+        select: function () {
+            this.onUpdate();
+        }
+    },
+    methods: {
+        serialise: function () {
+            return [this.variable, this.select];
+        },
+        deserialise: function (data) {
+            [this.variable, this.select] = data ? data : [null, null];
+        },
+        onUpdate: function () {
+            this.$emit('update', this.serialise());
+        },
+        onDelete: function () {
+            this.$emit('pop');
+        },
+        onExecute: function (runner) {
+            if(this.variable) {
+                if(runner.hasValue(this.variable)) {
+                    this.select = runner.getValue(this.variable).data;
+                } else {
+                    runner.setValue(this.variable, runner.createValue(this.select));
+                }
+            }
+        }
+    }
+});
