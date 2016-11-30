86145adac1b1629754de8a3e3de639dcf546468d
Mathieu Ledru <matyo91@gmail.com>
2016-11-30 22:43:38 +0100

    checkboxes 🙂

--- web/js/uniflow/components.js
+++ web/js/uniflow/components.js
@@ -1,14 +1,11 @@
 import CoreJavascript from './core/javascript/index.js';
 import CoreAssets from './core/assets/index.js';
-import CoreSFTP from './core/sftp/index.js';
 import CoreText from './core/text/index.js';
-import CoreYAML from './core/yaml/index.js';
-import CoreReplace from './core/replace/index.js';
-
-import DecleorTrads from './old/trads/index.js';
+import UICheckBoxes from './ui/checkboxes/index.js';
 
 export default {
     'core-javascript': CoreJavascript,
     'core-assets': CoreAssets,
     'core-text': CoreText,
+    'ui-checkboxes': UICheckBoxes,
 };
