351d52f6cfd2b028de8ae8f01d115a2fe0db50e6
Mathieu Ledru <matyo91@gmail.com>
2017-04-19 19:43:06 +0200

    😉 text sentry exception

--- src/Darkwood/FrontBundle/Controller/DefaultController.php
+++ src/Darkwood/FrontBundle/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace Darkwood\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -31,7 +30,6 @@ class DefaultController extends Controller
 
     public function homeAction(Request $request)
     {
-        throw new Exception();
         return $this->render('FrontBundle::layout.html.twig');
     }
 }
