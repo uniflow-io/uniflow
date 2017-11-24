b6f429a585a247dc1671ec89eb20a26c98689a52
Mathieu Ledru <matyo91@gmail.com>
2017-11-24 17:16:43 +0100

    😀 composer update

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 const Server = require('socket.io');
 const io = new Server();
+const fs = require('fs');
 
 io.on('connection', function(socket){
     console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
     });
-    socket.on('message', function(msg){
-        io.emit('message', msg);
+    socket.on('read', function(path){
+        fs.readFile(path, 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            io.emit('read', data);
+        });
     });
 });
 
