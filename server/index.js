f4c094b5f9f58195d21d00165b98caeac40a06ff
Mathieu Ledru <matyo91@gmail.com>
2017-11-28 00:09:02 +0100

    🙂 server

--- server/index.js
+++ server/index.js
@@ -1,19 +1,38 @@
+const fs = require('fs-extra')
 const Server = require('socket.io');
 const io = new Server();
-const fs = require('fs');
 
-io.on('connection', function(socket){
-    console.log('a user connected');
+io.on('connection', function (socket) {
+    /*console.log('a user connected');
     socket.on('disconnect', function(){
         console.log('user disconnected');
-    });
-    socket.on('read', function(path, callback){
-        fs.readFile(path, 'utf8', function (err,data) {
+    });*/
+
+    socket.on('read', function (path, callback) {
+        fs.readFile(path, 'utf8', function (err, data) {
             if (err) {
                 return console.log(err);
             }
             callback(data)
         });
+    });
+
+    socket.on('write', function (path, content, callback) {
+        fs.writeFile(path, content, 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            callback(data)
+        });
+    });
+
+    socket.on('copy', function (pathFrom, pathTo, callback) {
+        fs.copy(pathFrom, pathTo, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            callback(true)
+        })
     });
 });
 
