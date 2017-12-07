5feb96cda97d14532af4f7f90889631fdb8768b7
Mathieu Ledru <matyo91@gmail.com>
2017-12-07 19:30:41 +0100

    😊 robotjs

--- server/components/robot.js
+++ server/components/robot.js
@@ -1,98 +1,117 @@
 const robot = require('robotjs');
 
+let applyArgs = function(args) {
+    let realArgs = []
+
+    for(let i = 0; i < args.length; i++) {
+        let canApply = false
+        for(let j = i; j < args.length; j++) {
+            canApply = canApply || args[j] !== null
+        }
+
+        if(!canApply) {
+            return realArgs;
+        }
+
+        realArgs.push(args[i])
+    }
+
+    return realArgs
+}
+
 module.exports = function(socket) {
     socket.on('robot.keyboard.setDelay', function (ms, callback) {
-        robot.setKeyboardDelay(ms)
+        robot.setKeyboardDelay.apply(robot, applyArgs([ms]))
 
         callback()
     })
 
     socket.on('robot.keyboard.keyTap', function (key, modifier, callback) {
-        robot.keyTap(key, modifier)
+        robot.keyTap.apply(robot, applyArgs([key, modifier]))
 
         callback()
     })
 
     socket.on('robot.keyboard.keyToggle', function (key, down, modifier, callback) {
-        robot.keyToggle(key, down, modifier)
+        robot.keyToggle.apply(robot, applyArgs([key, down, modifier]))
 
         callback()
     })
 
     socket.on('robot.keyboard.typeString', function (string, callback) {
-        robot.typeString(string)
+        robot.typeString.apply(robot, applyArgs([string]))
 
         callback()
     })
 
     socket.on('robot.keyboard.typeStringDelayed', function (string, cpm, callback) {
-        robot.typeStringDelayed(string, cpm)
+        robot.typeStringDelayed.apply(robot, applyArgs([string, cpm]))
 
         callback()
     })
 
     socket.on('robot.mouse.setDelay', function (ms, callback) {
-        robot.setMouseDelay(ms)
+        robot.setMouseDelay.apply(robot, applyArgs([ms]))
 
         callback()
     })
 
     socket.on('robot.mouse.move', function (x, y, callback) {
-        robot.moveMouse(x, y)
+        robot.moveMouse.apply(robot, applyArgs([x, y]))
 
         callback()
     })
 
     socket.on('robot.mouse.moveSmooth', function (x, y, callback) {
-        robot.moveMouseSmooth(x, y)
+        robot.moveMouseSmooth.apply(robot, applyArgs([x, y]))
 
         callback()
     })
 
     socket.on('robot.mouse.click', function (button = 'left', double = false, callback) {
-        robot.mouseClick(button, double)
+        robot.mouseClick.apply(robot, applyArgs([button, double]))
 
         callback()
     })
 
     socket.on('robot.mouse.toggle', function (down = 'down', button = 'left', callback) {
-        robot.mouseToggle(down, button)
+        robot.mouseToggle.apply(robot, applyArgs([down, button]))
 
         callback()
     })
 
     socket.on('robot.mouse.drag', function (x, y, callback) {
-        robot.dragMouse(x, y)
+        robot.dragMouse.apply(robot, applyArgs([x, y]))
 
         callback()
     })
 
     socket.on('robot.mouse.getPos', function (callback) {
-        let mouse = robot.getMousePos()
+        let mouse = robot.getMousePos.apply(robot, applyArgs([]))
 
         callback(mouse)
     })
 
     socket.on('robot.mouse.scroll', function (x, y, callback) {
-        robot.scrollMouse(x, y)
+        robot.scrollMouse.apply(robot, applyArgs([x, y]))
 
         callback()
     })
 
     socket.on('robot.screen.getPixelColor', function (x, y, callback) {
-        robot.scrollMouse(x, y)
+        robot.scrollMouse.apply(robot, applyArgs([x, y]))
 
         callback()
     })
 
     socket.on('robot.screen.getScreenSize', function (callback) {
-        let data = robot.getScreenSize()
+        let data = robot.getScreenSize.apply(robot, applyArgs([]))
 
         callback(data)
     })
 
     socket.on('robot.screen.capture', function (x, y, width, height, callback) {
-        let img = robot.robot.screen.capture(x, y, width, height)
+        let img = robot.robot.screen.capture.apply(robot, applyArgs([x, y, width, height]))
 
         callback(img)
     })
