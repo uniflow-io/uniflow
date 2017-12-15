1044839d121314f251889dd178338b13b3e99cc2
Mathieu Ledru <matyo91@gmail.com>
2017-12-15 16:11:49 +0100

    😀 begin react

---
+++ old2/app.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+import store from './store.js'
+import router from './router.js'
+
+import './components/select2.js'
+import './components/tagit.js'
+import './components/ace.js'
+import './components/icheckbox.js'
+
+import LayoutSection from './sections/layout/index.js'
+
+const app = new Vue(Vue.util.extend({
+    store: store,
+    router: router
+}, LayoutSection));
+app.$mount('#app');
