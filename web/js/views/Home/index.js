c1d7dbcd8c1675f92754dc6e5f7e758a6728f561
Mathieu Ledru <matyo91@gmail.com>
2017-12-20 13:37:23 +0100

    🙂 begin-home

--- web/js/views/Home/index.js
+++ web/js/views/Home/index.js
@@ -1,7 +1,34 @@
 import React from 'react'
 
 export default () => (
-    <div>
-        home
+    <div id="home" className="content-wrapper">
+        {/* Content Header (Page header) */}
+        <section className="content-header">
+            <h1>
+                Dashboard
+                <small>Control panel</small>
+            </h1>
+            <ol className="breadcrumb">
+                <li><a href="#"><i className="fa fa-dashboard"></i> Home</a></li>
+                <li className="active">Dashboard</li>
+            </ol>
+        </section>
+
+        {/* Main content */}
+        <section className="content">
+            <div className="row">
+                <div className="col-md-2">
+
+                    <history />
+
+                </div>
+                <div className="col-md-10">
+
+                    <show v-if="history" />
+
+                </div>
+            </div>
+        </section>
+        {/* /.content */}
     </div>
 )
