2415753f4c82e782618a0c2b41cef801b8418752
Mathieu Ledru <matyo91@gmail.com>
2017-12-20 13:30:31 +0100

    😀 faq

--- web/js/views/FAQ/index.js
+++ web/js/views/FAQ/index.js
@@ -1,7 +1,74 @@
 import React from 'react'
 
 export default () => (
-    <div>
-        faq
+    <div class="content-wrapper">
+        {/* Content Header (Page header) */}
+        <section class="content-header">
+            <h1>
+                FAQ
+                <small>Control panel</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">FAQ</li>
+            </ol>
+        </section>
+
+        {/* Main content */}
+        <section class="content">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <h3>Why ?</h3>
+                    {/*p>
+                        We all have someone or multi-person that will continously harras you at work for no reason.
+                        Just for pretending working and for having this ego fullfilled with the team and impress his boss to gain only at the end 0.5% pay rise per year, he will use you and ask you anything possible.
+                        He has no idea how to resolve the issue, but you stick a part of the whole hierachy then you have no choice but keep going to smile and accept the dirty work.
+                        Meanwhile every member of the team is playing nerf in a good mood at the open-space, you are not. It's the kiss-kiss effect of the team, every body have to show his/her coolness to show people that we are social.
+                        But you have to pretend working and you just don't give a single shit of your work. In fact you are alone between the keyboard and your chair and no person will make your work. And every body will pretend having to much to do, but do nothing in reality.
+                        This is not told, but people really like making other people upset, then they make you the bad guy just by a smile or sentense that is lound pronouced in front of every body that you just can't argue.
+                        But you did the whole job and no one will reward your work. Only the moments your are complaning are remembered and put on the table at your annual interview for not giving you any value and keep the positions.
+                        It's like master / slave communication and you never, ever can't break the rule since you have not a position in the company.
+                        You are stuck to work gain the dam pay month money and for preserving your survival in society by feeding in money the people who do the less and claim all the team work.
+                        Technology pretend to improve your life, but in fact it's just people that want's always more. Not for you, but for them, the faster you act, the common you become, then you are well identified and people assume that you have to be fastest.
+                    </p>
+                    <p>
+                        So that's how this projet come to live. The goal is to delegate. Delegate to who ? The computer ...
+                        The computer may be the only domain that is virtual. Counter to the contruction builing for instance where you have to do real things. Here you have a computer in your hand, and you easily can create, destroy, do, cancel, redo things in one mouse click. In fact, this is just data, manipulating data and produce something at the end.
+                        In your dirty work you will identify parts of your jobs that you can reproduce, then automate micro tasks in for your daily and boring todo list that you never care but end the day and go home.
+                    </p*/}
+                    <p>Do nothing !</p>
+
+                    <h3>How ?</h3>
+                    <p>Think macro, act micro</p>
+
+                    <h3>Mono Flow, the inspiration</h3>
+                    <p>
+                        I did realy like, and a very fan of NoFlow or Workflow for iOS, IFTTT, Automator, Zapier, Huggin, google Blocky.
+                        The flow programming is very interresting, because you assemble blocs like legos.
+                        What can become complicated is when you assemble too much and you got a graph like spagety.
+                        So i did make a choice to make it human readable and have your flow ordered by a single list, as human we like list it's common, anchor into our usage, affordable and natural to read from top to bottom.
+                        At the end you can make anything into each blocks, so have conditions, loops and so on as long as something is delivered at the end.
+                        For the interface, i also did pickup the philopsophy of nvALT
+                    </p>
+
+                    <h3>Javascript</h3>
+                    <p>
+                        I specialy choose Javascript, because it's actualy for me the only language that is large covered for any device this can work on browser, mobile and desktop and can have native part bridge integration.
+                        More over Javascript has a very simple curve to learn and can also be high to master.
+                    </p>
+
+                    <h3>What content it deserve</h3>
+                    <p>
+                        The goal is to deserve realy anything. So a block can be a simple text transformation as it can be a very specific process.
+                        The harmony is important for having a base to allow mix blocks together.
+                        Only the security is the top most priority to be checked.
+                    </p>
+
+                </div>
+            </div>
+        </section>
+        {/* /.content */}
     </div>
+
 )
