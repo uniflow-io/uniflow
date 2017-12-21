445e80c6978a04e227a3d1b147dc1828f3a076b0
Mathieu Ledru <matyo91@gmail.com>
2017-12-21 17:23:56 +0100

    😀 history

--- web/js/routes.js
+++ web/js/routes.js
@@ -1,4 +1,5 @@
 import { FAQ, Home } from './views/index'
+import pathToRegexp from 'path-to-regexp'
 
 const routes = {
     home: {
@@ -18,6 +19,14 @@ const routes = {
         path: '/detail/:id',
         component: Home,
     },
+}
+
+export const pathTo = (view, params = {}) => {
+    if (!(view in routes)) {
+        throw new Error(`There is no such view as ${view}`)
+    }
+
+    return pathToRegexp.compile(routes[view].path)(params)
 }
 
 export default routes
--- web/js/views/Home/History/index.js
+++ web/js/views/Home/History/index.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {pathTo} from 'uniflow/routes'
+import moment from 'moment'
 
-let id = 1;
+let id = 2;
 
 export default class History extends Component {
     state = {
         search: '',
-        items: {},
+        items: {
+            1: {
+                id: '1',
+                title: 'coucou',
+                tags: ['deed'],
+                description: 'description',
+                updated: moment()
+            }
+        },
         current: null
     }
 
@@ -15,9 +26,43 @@ export default class History extends Component {
 
     onSubmit = (event) => {
         event.preventDefault();
+
+
+
+        id ++;
     }
 
     render() {
+        const filteredHistory = (state) => {
+            let keys = Object.keys(state.items);
+
+            if (state.search) {
+                keys = keys.filter((key) => {
+                    let item  = state.items[key];
+                    let words = item.title;
+                    words += item.tags.join(' ');
+                    words     = words.toLowerCase();
+
+                    return words.indexOf(this.search) !== -1;
+                });
+            }
+
+            keys.sort((keyA, keyB) => {
+                let itemA = state.items[keyA],
+                    itemB = state.items[keyB];
+
+                return itemB.updated.diff(itemA.updated);
+            });
+
+            return keys.map((key) => {
+                return state.items[key];
+            });
+        }
+
+        const isActive = (state, item) => {
+            return (state.current && state.curent.id === item.id) ? 'active' : ''
+        }
+
         return (
             <div className="box box-danger">
                 <div className="box-header with-border">
@@ -26,11 +71,12 @@ export default class History extends Component {
                 <div className="box-body">
                     <div className="navbar navbar-default navbar-vertical" role="navigation">
                         <div className="navbar-header">
-                            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                            <button type="button" className="navbar-toggle" data-toggle="collapse"
+                                    data-target=".sidebar-navbar-collapse">
                                 <span className="sr-only">Toggle navigation</span>
-                                <span className="icon-bar" />
-                                <span className="icon-bar" />
-                                <span className="icon-bar" />
+                                <span className="icon-bar"/>
+                                <span className="icon-bar"/>
+                                <span className="icon-bar"/>
                             </button>
                             <span className="visible-xs navbar-brand">Sidebar menu</span>
                         </div>
@@ -38,17 +84,25 @@ export default class History extends Component {
                             <ul className="nav navbar-nav">
                                 <li>
                                     <form className="navbar-form" role="search" onSubmit={this.onSubmit}>
-                                        <div className="input-group" style={{ width: '100%'}}>
-                                            <input type="text" className="form-control" placeholder="Search" value={this.state.search} onChange={this.onChange} />
+                                        <div className="input-group" style={{width: '100%'}}>
+                                            <input type="text" className="form-control" placeholder="Search"
+                                                   value={this.state.search} onChange={this.onChange}/>
                                             {/*div className="input-group-btn">
                                                 <button className="btn btn-default" type="submit"><i className="glyphicon glyphicon-search"></i></button>
                                             </div*/}
                                         </div>
                                     </form>
                                 </li>
-                                {/*<li v-for="item in filteredHistory" :className="{'active':(history && item.id == history.id)}"><router-link :to="{ name: 'homeDetail', params: { id: item.id }}">{{ item.title }} <span v-for="tag in item.tags" className="badge">{{ tag }}</span></router-link></li>*/}
+                                {filteredHistory(this.state).map((item, i) => (
+                                    <li className={isActive(this.state, item)} key={i}>
+                                        <Link to={pathTo('homeDetail', {id: item.id})}>{item.title} {item.tags.map((tag, j) => (
+                                            <span key={j} className="badge">{tag}</span>
+                                        ))}</Link>
+                                    </li>
+                                ))}
                             </ul>
-                        </div>{/*/.nav-collapse */}
+                        </div>
+                        {/*/.nav-collapse */}
                     </div>
                 </div>
             </div>
