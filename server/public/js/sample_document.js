1a55fd92acf652b9365c60f45320d60cc5e83404
Mathieu Ledru <matyo91@gmail.com>
2018-04-09 17:08:58 +0200

    yaml

--- server/public/js/sample_document.js
+++ server/public/js/sample_document.js
@@ -5,15 +5,15 @@
 var fs   = require('fs');
 var path = require('path');
 var util = require('util');
-var yaml = require('../js/js-yaml');
+var yaml = require('./js-yaml');
 
 
 try {
-  var filename = path.join(__dirname, 'sample_document.yml'),
+  var filename = path.join(__dirname, '/../assets/sample_document.yml'),
       contents = fs.readFileSync(filename, 'utf8'),
       data     = yaml.load(contents);
 
-  console.log(util.inspect(data, false, 10, true));
+  //console.log(util.inspect(data, false, 10, true));
 } catch (err) {
   console.log(err.stack || String(err));
 }
