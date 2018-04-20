4756af712b1bee5e18aa64cff0b1c1f9b3fa49ee
Mathieu Ledru <matyo91@gmail.com>
2018-04-20 10:53:19 +0200

    rm old & old2

--- old2/messages/search.js
+++
@@ -1,8 +0,0 @@
-class SearchMessage {
-    constructor(options, message) {
-        this.message = message;
-        this.options = options;
-    }
-}
-
-export default SearchMessage;
--- old2/sections/faq/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import template from './template.html!text'
-
-export default Vue.extend({
-    template: template
-});
