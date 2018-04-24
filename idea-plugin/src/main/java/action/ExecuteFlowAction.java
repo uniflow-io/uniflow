fe9384b98fb6b675798912f610efa3245d7803cb
Mathieu Ledru <matyo91@gmail.com>
2018-04-24 18:24:23 +0200

    Add action

---
+++ idea-plugin/src/main/java/action/ExecuteFlowAction.java
@@ -0,0 +1,17 @@
+package action;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAwareAction;
+
+import java.awt.*;
+
+public class ExecuteFlowAction extends DumbAwareAction {
+
+    public ExecuteFlowAction() {
+        super("Execute Flow");
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+        return;
+    }
+}
