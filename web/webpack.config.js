a5ceac362d5190f3e701a37f4a33fdcc25eafbc5
Mathieu Ledru <matyo91@gmail.com>
2018-04-26 14:36:54 +0200

    add webpack

---
+++ web/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production'
+};
