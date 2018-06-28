38a3dd29e9d2a8b7741dd8df73607962f0daac76
Mathieu Ledru <matyo91@gmail.com>
2018-06-28 16:29:11 +0200

    move-front

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('default/home.html.twig');
+    }
+}
