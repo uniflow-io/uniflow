f2d54bdd32b561392dd71aeb1808eee8fda89b94
Mathieu Ledru <matyo91@gmail.com>
2018-07-04 16:56:57 +0200

    Add sentry

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
