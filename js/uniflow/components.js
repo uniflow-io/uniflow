1ccb4a91dfd6ed388deb03b4e9d7becd94501949
Mathieu Ledru <matyo91@gmail.com>
2018-07-10 16:02:38 +0200

    RM url

--- js/uniflow/components.js
+++ js/uniflow/components.js
@@ -9,7 +9,6 @@ import ComponentSelect from './components/select.js';
 import ComponentSocketIO from './components/socketio.js';
 import ComponentText from './components/text.js';
 import ComponentTextList from './components/textlist.js';
-import ComponentUrl from './components/url.js';
 import ComponentWhile from "./components/while.js";
 import ComponentYamlAdd from "./components/yaml-add.js";
 
@@ -25,7 +24,6 @@ export default {
     'socketio': ComponentSocketIO,
     'text': ComponentText,
     'textlist': ComponentTextList,
-    'url': ComponentUrl,
     'while': ComponentWhile,
     'yaml-add': ComponentYamlAdd
 }
