9027b66604b04bce7e1fe6e099b04b3ae1e1a64a
Mathieu Ledru <matyo91@gmail.com>
2018-07-17 11:08:17 +0200

    run

--- bash/run.js
+++ bash/run.js
@@ -1,1 +1,7 @@
-console.log('coucou')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+exec('ls')
+    .then(function(result) {
+        console.log(result)
+    })
