11de4b500d2c1956422652468beb90464274adf6
Mathieu Ledru <matyo91@gmail.com>
2018-07-20 16:06:15 +0200

    Add user profile

--- js/reducers/user/actions.js
+++ js/reducers/user/actions.js
@@ -3,6 +3,7 @@ import server from '../../utils/server'
 import components from '../../uniflow/components'
 import {
     COMMIT_SET_COMPONENTS,
+    COMMIT_UPDATE_PROFILE,
 } from './actionsTypes'
 
 export const fetchComponents = () => {
@@ -16,6 +17,30 @@ export const fetchComponents = () => {
             });
     }
 }
+export const fetchProfile = () => {
+    return (dispatch) => {
+        return request
+            .get(server.getBaseUrl() + '/user/profile')
+            .then((response) => {
+                dispatch(commitUpdateProfile(response.data));
+            });
+    }
+}
+export const updateProfile = (item) => {
+    return (dispatch) => {
+        let data = {
+            apiKey: item.apiKey
+        };
+
+        return request
+            .post(server.getBaseUrl() + '/user/profile', data)
+            .then((response) => {
+                dispatch(commitUpdateProfile(data));
+
+                return data;
+            });
+    }
+}
 export const commitSetComponents = (components) => {
     return (dispatch) => {
         dispatch({
@@ -25,3 +50,13 @@ export const commitSetComponents = (components) => {
         return Promise.resolve()
     }
 }
+export const commitUpdateProfile = (item) => {
+    return (dispatch) => {
+        dispatch({
+            type: COMMIT_UPDATE_PROFILE,
+            item
+        })
+        return Promise.resolve()
+    }
+}
+
--- js/reducers/user/index.js
+++ js/reducers/user/index.js
@@ -1,9 +1,11 @@
 import {
     COMMIT_SET_COMPONENTS,
+    COMMIT_UPDATE_PROFILE,
 } from './actionsTypes'
 
 const defaultState = {
-    components: {}
+    components: {},
+    apiKey: null,
 }
 
 const user = (state = defaultState, action) => {
@@ -12,6 +14,11 @@ const user = (state = defaultState, action) => {
             return {
                 ...state,
                 ...{components: action.components}
+            }
+        case COMMIT_UPDATE_PROFILE:
+            return {
+                ...state,
+                ...{apiKey: action.apiKey}
             }
         default:
             return state
