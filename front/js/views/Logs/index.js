a62339575f5f0b4f1f433d2c52c88fb19414a112
Mathieu Ledru <matyo91@gmail.com>
2018-08-05 19:02:37 +0200

    Add prompt component

--- front/js/views/Logs/index.js
+++ front/js/views/Logs/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const logs = [{
+    tag: '0.21',
+    label: 'Add prompt component',
+    date: '05/08/2018'
+}, {
     tag: '0.20',
     label: 'Add bash runner',
     date: '26/07/2018'
