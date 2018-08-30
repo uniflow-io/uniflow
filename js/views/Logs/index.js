24b2c6b624d34815ed4e787fac3a79f0dbafb3fb
Mathieu Ledru <matyo91@gmail.com>
2018-08-30 14:51:59 +0200

    Add platform to history

--- js/views/Logs/index.js
+++ js/views/Logs/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
 const logs = [{
+    tag: '0.23',
+    label: 'Add platform to history',
+    date: '30/08/2018'
+}, {
     tag: '0.22',
     label: 'Add regex component',
     date: '17/08/2018'
