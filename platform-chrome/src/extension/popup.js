70d45c460b0de89e50063cde38aa4376d70d607d
Mathieu Ledru <matyo91@gmail.com>
2018-08-31 17:29:43 +0200

    extension

---
+++ platform-chrome/src/extension/popup.js
@@ -0,0 +1,12 @@
+(function () {
+    function refresh() {
+        let api         = new Api('prod', 'qNFN9tqqg0tKq7GMPQy1r3nskFMntRjc')
+        api.endpoint('history')
+            .then((response) => {
+                console.log(response)
+            })
+        console.log('refresh')
+    }
+
+    refresh()
+})()
