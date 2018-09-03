71089853d20df5f3719256a1001a7c3d54d8c8a0
Mathieu Ledru <matyo91@gmail.com>
2018-09-03 17:07:25 +0200

    Add platform chrome

--- src/extension/popup.js
+++ src/extension/popup.js
@@ -1,14 +1,13 @@
 import Api from '../models/Api'
 
 (function () {
-    function refresh() {
-        let api         = new Api('prod', 'qNFN9tqqg0tKq7GMPQy1r3nskFMntRjc')
+    const refreshButton = document.getElementById('refresh');
+
+    refreshButton.addEventListener('click', () => {
+        let api = new Api('prod', 'qNFN9tqqg0tKq7GMPQy1r3nskFMntRjc')
         api.endpoint('history')
             .then((response) => {
                 console.log(response)
             })
-        console.log('refresh')
-    }
-
-    refresh()
+    });
 })()
