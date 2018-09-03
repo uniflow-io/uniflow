be29210aed6c9dfdd91b6e41c4ebc5b6ccbe47c1
Mathieu Ledru <matyo91@gmail.com>
2018-09-03 18:25:48 +0200

    Save options

--- src/extension/options.js
+++ src/extension/options.js
@@ -1,10 +1,11 @@
 // Saves options to chrome.storage
 function save_options() {
-    var color = document.getElementById('color').value;
-    var likesColor = document.getElementById('like').checked;
+    var apiKey = document.getElementById('apiKey').value;
+    var env = document.getElementById('env').value;
+
     chrome.storage.sync.set({
-        favoriteColor: color,
-        likesColor: likesColor
+        apiKey: apiKey,
+        env: env
     }, function() {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -18,15 +19,14 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    // Use default value color = 'red' and likesColor = true.
+    // Use default value apiKey = 'red' and env = true.
     chrome.storage.sync.get({
-        favoriteColor: 'red',
-        likesColor: true
+        apiKey: '',
+        env: 'prod'
     }, function(items) {
-        document.getElementById('color').value = items.favoriteColor;
-        document.getElementById('like').checked = items.likesColor;
+        document.getElementById('apiKey').value = items.apiKey;
+        document.getElementById('env').value = items.env;
     });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.getElementById('save').addEventListener('click', save_options);
