e4149ee4a760b98374774083ab3343dc431703df
Mathieu Ledru <matyo91@gmail.com>
2018-09-03 17:35:56 +0200

    extension

--- platform-chrome/src/extension/popup.js
+++ platform-chrome/src/extension/popup.js
@@ -2,12 +2,24 @@ import Api from '../models/Api'
 
 (function () {
     const refreshButton = document.getElementById('refresh');
+    const historyElement = document.getElementById('history');
 
-    refreshButton.addEventListener('click', () => {
+    const refresh = () => {
         let api = new Api('prod', 'qNFN9tqqg0tKq7GMPQy1r3nskFMntRjc')
         api.endpoint('history')
             .then((response) => {
-                console.log(response)
+                let items = []
+                for(let i = 0; i < response.data.length; i++) {
+                    let item = response.data[i]
+                    const template = `<div class="row"><div class="col-6-sm">${item.title}</div><div class="col-6-sm"><a href="${item.id}">run</a></div></div>`
+
+                    items.push(template)
+                }
+
+                historyElement.innerHTML = items.join('');
             })
-    });
+    }
+
+    refreshButton.addEventListener('click', refresh);
+    refresh()
 })()
