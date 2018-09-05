5ef7db19384f0b75cdb16005d26d7d874c040d8f
Mathieu Ledru <matyo91@gmail.com>
2018-09-05 16:56:46 +0200

    Add chrome text component

--- src/uniflow/components.js
+++ src/uniflow/components.js
@@ -1,5 +1,7 @@
 import ComponentJavascript  from './components/javascript.js'
+import ComponentText  from './components/text.js'
 
 export default {
     'javascript': ComponentJavascript,
+    'text': ComponentText,
 }
