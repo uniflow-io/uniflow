33f233186df4fc68524a721899ab14defde46d11
Mathieu Ledru <matyo91@gmail.com>
2018-09-05 15:41:46 +0200

    chrome

--- src/uniflow/components/javascript.js
+++ src/uniflow/components/javascript.js
@@ -6,6 +6,10 @@ ComponentJavascript.prototype.deserialise = function(data) {
     this.javascript = data
 }
 
+ComponentJavascript.prototype.onCompile = function(interpreter, scope, asyncWrapper) {
+
+}
+
 ComponentJavascript.prototype.onExecute = function(runner) {
     return runner.eval(this.javascript)
 }
