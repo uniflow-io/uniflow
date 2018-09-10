94049581eca1538da41a08b99d9969e7d094bc90
Mathieu Ledru <matyo91@gmail.com>
2018-09-10 17:00:56 +0200

    chrome

--- platform-chrome/src/uniflow/components/chrome.js
+++ platform-chrome/src/uniflow/components/chrome.js
@@ -1,4 +1,6 @@
-export default class ComponentText {
+const axios      = require('axios');
+
+export default class ComponentChrome {
     constructor() {
         this.variable = null
     }
@@ -10,12 +12,52 @@ export default class ComponentText {
     }
 
     onCompile(interpreter, scope, asyncWrapper) {
+        let obj = {};
 
+        let constructorWrapper  = function () {
+            let newChrome  = interpreter.createObjectProto(obj.BROWSER_PROTO),
+                wrapper;
+
+            wrapper = function (url, config, callback) {
+                return axios.get(url, config).then((response) => {
+                    url = interpreter.pseudoToNative(url)
+                    config = interpreter.pseudoToNative(config)
+
+                    callback(interpreter.nativeToPseudo(response.data))
+                })
+            };
+            interpreter.setProperty(newChrome, 'httpGet', interpreter.createAsyncFunction(asyncWrapper(wrapper)));
+
+            wrapper = function (url, data, config, callback) {
+                url = interpreter.pseudoToNative(url)
+                data = interpreter.pseudoToNative(data)
+                config = interpreter.pseudoToNative(config)
+
+                return axios.post(url, data, config).then((response) => {
+                    callback(interpreter.nativeToPseudo(response.data))
+                })
+            };
+            interpreter.setProperty(newChrome, 'httpPost', interpreter.createAsyncFunction(asyncWrapper(wrapper)));
+
+            wrapper = function (url, data, config, callback) {
+                url = interpreter.pseudoToNative(url)
+                data = interpreter.pseudoToNative(data)
+                config = interpreter.pseudoToNative(config)
+
+                return axios.put(url, data, config).then((response) => {
+                    callback(interpreter.nativeToPseudo(response.data))
+                })
+            };
+            interpreter.setProperty(newChrome, 'httpPut', interpreter.createAsyncFunction(asyncWrapper(wrapper)));
+
+            return newChrome;
+        };
+        obj.Chrome       = interpreter.createNativeFunction(constructorWrapper, true);
+        obj.BROWSER_PROTO = interpreter.getProperty(obj.Chrome, 'prototype');
+        interpreter.setProperty(scope, 'Chrome', obj.Chrome);
     }
 
     onExecute(runner) {
-        if (this.variable) {
-            runner.setValue(this.variable, 'chrome');
-        }
+        return runner.eval('var ' + this.variable + ' = new Chrome()')
     }
 }
