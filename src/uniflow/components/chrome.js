509bd5182548db7149d129fae118453531bbdf72
Mathieu Ledru <matyo91@gmail.com>
2018-09-11 14:53:44 +0200

    Add browserEvaluateInBackground

--- src/uniflow/components/chrome.js
+++ src/uniflow/components/chrome.js
@@ -1,5 +1,22 @@
 const axios      = require('axios');
 
+const serializeFunction = (f) => {
+    const serialized = f.toString();
+
+    // Safari serializes async arrow functions with an invalid `function` keyword.
+    // This needs to be removed in order for the function to be interpretable.
+    const safariPrefix = 'async function ';
+    if (serialized.startsWith(safariPrefix)) {
+        const arrowIndex = serialized.indexOf('=>');
+        const bracketIndex = serialized.indexOf('{');
+        if (arrowIndex > -1 && (bracketIndex === -1 || arrowIndex < bracketIndex)) {
+            return `async ${serialized.slice(safariPrefix.length)}`;
+        }
+    }
+
+    return serialized;
+};
+
 export default class ComponentChrome {
     constructor() {
         this.variable = null
@@ -49,6 +66,32 @@ export default class ComponentChrome {
                 })
             };
             interpreter.setProperty(newChrome, 'httpPut', interpreter.createAsyncFunction(asyncWrapper(wrapper)));
+
+            wrapper = function (asyncFunction, args, callback) {
+                asyncFunction = interpreter.pseudoToNative(asyncFunction)
+                asyncFunction = serializeFunction(asyncFunction)
+                args = interpreter.pseudoToNative(args)
+
+                return Promise.resolve()
+                    .then(() => {
+                        return eval(`(${asyncFunction}).apply(null, ${JSON.stringify(args)})`)
+                    }).then((result) => {
+                        callback(interpreter.nativeToPseudo(result));
+                    })
+            };
+            interpreter.setProperty(newChrome, 'browserEvaluateInBackground', interpreter.createAsyncFunction(asyncWrapper(wrapper)));
+
+            wrapper = function (tabId, asyncFunction, args, callback) {
+                tabId = interpreter.pseudoToNative(tabId)
+                asyncFunction = interpreter.pseudoToNative(asyncFunction)
+                asyncFunction = serializeFunction(asyncFunction)
+                args = interpreter.pseudoToNative(args)
+                return browser.evaluateInContent(tabId, asyncFunction, args)
+                    .then((result) => {
+                        callback(interpreter.nativeToPseudo(result));
+                    })
+            };
+            interpreter.setProperty(newChrome, 'browserEvaluateInContent', interpreter.createAsyncFunction(asyncWrapper(wrapper)));
 
             return newChrome;
         };
