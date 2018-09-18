17243c099406293c5c65d794df1472ec3b2d8c7b
Mathieu Ledru <matyo91@gmail.com>
2018-09-18 16:36:32 +0200

    Add JWT authentification

--- back/src/Controller/SecurityController.php
+++ back/src/Controller/SecurityController.php
@@ -2,42 +2,22 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\Exception\LogicException;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
-     * @param Request $request
-     * @param AuthenticationUtils $authenticationUtils
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @Route("/login", name="login")
-     */
-    public function login(Request $request, AuthenticationUtils $authenticationUtils)
-    {
-        // get the login error if there is one
-        /*$error = $authenticationUtils->getLastAuthenticationError();
-
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));*/
-
-        return $this->render('default/login.html.twig');
-    }
-
-    /**
      * @throws \Exception
      *
-     * @Route("/logout", name="logout")
+     * @Route("/api/login_check", name="api_login_check")
      */
-    public function logout()
+    public function loginCheck(Request $request)
     {
         throw new LogicException('This should never be reached!');
     }
