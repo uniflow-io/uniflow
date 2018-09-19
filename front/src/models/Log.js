2b87d44440de834ea483ed21a0d1a7b3e69aaf7d
Mathieu Ledru <matyo91@gmail.com>
2018-09-19 16:12:04 +0200

    log

--- front/src/models/Log.js
+++ front/src/models/Log.js
@@ -3,6 +3,7 @@ import moment from 'moment'
 export default class Log {
     static get HISTORY_CREATE_FAIL() { return 1 }
     static get HISTORY_DATA_SET_FAIL() { return 2 }
+    static get USER_LOGIN_FAIL() { return 3 }
 
     constructor(data) {
         Object.assign(this, data);
