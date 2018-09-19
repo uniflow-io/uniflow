d0c1b8bd765b55933df9f8f0d1a3a4ff4fcedb5e
Mathieu Ledru <matyo91@gmail.com>
2018-09-19 17:05:21 +0200

    Refactor controlers

--- front/src/views/Flow/index.js
+++ front/src/views/Flow/index.js
@@ -1,44 +1,87 @@
-import React from 'react'
+import React, {Component} from 'react'
 import History from './History/index'
 import Show from './Show/index'
-import {getCurrentHistory} from '../../reducers/history/actions'
+import {fetchHistory, getCurrentHistory, setCurrentHistory} from '../../reducers/history/actions'
 import {connect} from 'react-redux'
+import routes, {pathTo} from "../../routes";
+import {withRouter, matchPath} from 'react-router'
 
-const Home = (props) => (
-    <div id="flow" className="content-wrapper">
-        {/* Content Header (Page header) */}
-        <section className="content-header">
-            <h1>
-                Dashboard
-                <small>Control panel</small>
-            </h1>
-            <ol className="breadcrumb">
-                <li><a href="#"><i className="fa fa-dashboard" /> Dashboard</a></li>
-                <li className="active">Flow</li>
-            </ol>
-        </section>
+class Flow extends Component<Props> {
+    componentDidMount() {
+        const {location, history} = this.props
 
-        <section className="content">
-            <div className="row">
-                <div className="col-sm-2">
+        this.props.dispatch(fetchHistory()).then(() => {
+            const flowMatch = matchPath(location.pathname, {
+                path: routes.flow.path,
+                exact: true
+            })
+            const dashboardMatch = matchPath(location.pathname, {
+                path: routes.dashboard.path,
+                exact: true
+            })
 
-                    <History />
+            if (flowMatch) {
+                const current = parseInt(flowMatch.params.id)
+                this.props.dispatch(setCurrentHistory(current))
+            } else if (dashboardMatch) {
+                let keys = Object.keys(this.props.items)
 
-                </div>
-                <div className="col-sm-10">
+                keys.sort((keyA, keyB) => {
+                    let itemA = this.props.items[keyA],
+                        itemB = this.props.items[keyB]
 
-                    {props.history && (
-                        <Show />
-                    )}
+                    return itemB.updated.diff(itemA.updated)
+                })
 
-                </div>
+                if (keys.length > 0) {
+                    let item = this.props.items[keys[0]]
+                    this.props.dispatch(setCurrentHistory(item.id))
+                        .then(() => {
+                            history.push(pathTo('flow', {id: item.id}))
+                        })
+                }
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div id="flow" className="content-wrapper">
+                {/* Content Header (Page header) */}
+                <section className="content-header">
+                    <h1>
+                        Dashboard
+                        <small>Control panel</small>
+                    </h1>
+                    <ol className="breadcrumb">
+                        <li><a href="#"><i className="fa fa-dashboard"/> Dashboard</a></li>
+                        <li className="active">Flow</li>
+                    </ol>
+                </section>
+
+                <section className="content">
+                    <div className="row">
+                        <div className="col-sm-2">
+
+                            <History/>
+
+                        </div>
+                        <div className="col-sm-10">
+
+                            {this.props.currentHistory && (
+                                <Show/>
+                            )}
+
+                        </div>
+                    </div>
+                </section>
             </div>
-        </section>
-    </div>
-)
+        )
+    }
+}
 
 export default connect(state => {
     return {
-        history: getCurrentHistory(state.history)
+        currentHistory: getCurrentHistory(state.history)
     }
-})(Home)
+})(withRouter(Flow))
