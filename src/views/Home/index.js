4a09fe6c3540d864d12a03837a1a24e8a08511a6
Mathieu Ledru <matyo91@gmail.com>
2018-09-20 16:47:53 +0200

    home

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-const Home = (props) => (
-    <div id="home" className="content-wrapper">
-        <section className="content-header">
-            <h1>
-                Home
-                <small>Control panel</small>
-            </h1>
-            <ol className="breadcrumb">
-                <li><a href="#"><i className="fa fa-dashboard"/> Home</a></li>
-            </ol>
-        </section>
-    </div>
-)
+class Home extends Component {
+
+    render() {
+        return (
+            <div id="home" className="content-wrapper">
+                <section className="content-header">
+                    <h1>
+                        Home
+                        <small>Control panel</small>
+                    </h1>
+                    <ol className="breadcrumb">
+                        <li><a href="#"><i className="fa fa-dashboard"/> Home</a></li>
+                    </ol>
+                </section>
+            </div>
+        )
+    }
+
+}
 
 export default Home
