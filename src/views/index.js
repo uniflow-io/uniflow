d97c6bfef4523368f3c9f22debf805df21f2314c
Mathieu Ledru <matyo91@gmail.com>
2018-09-21 18:04:22 +0200

    register

--- src/views/index.js
+++ src/views/index.js
@@ -2,7 +2,8 @@ import FAQ from './FAQ/index'
 import Logs from './Logs/index'
 import Home from './Home/index'
 import Login from './Login/index'
+import Register from './Register/index'
 import Flow from './Flow/index'
 import Settings from './Settings/index'
 
-export { FAQ, Logs, Home, Login, Flow, Settings }
+export { FAQ, Logs, Home, Login, Register, Flow, Settings }
