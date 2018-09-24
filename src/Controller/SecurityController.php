13dd81687dd7fa2fc9e56193fe409e8480521fd2
Mathieu Ledru <matyo91@gmail.com>
2018-09-24 18:07:25 +0200

    Add register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use App\Services\UserService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\LogicException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
+    /**
+     * @var UserService
+     */
+    protected $userService;
+
+    public function __construct(
+        UserService $userService
+    )
+    {
+        $this->userService = $userService;
+    }
     /**
      * @throws \Exception
      *
@@ -20,5 +35,45 @@ class SecurityController extends Controller
     public function loginCheck(Request $request)
     {
         throw new LogicException('This should never be reached!');
+    }
+
+    /**
+     * @throws \Exception
+     *
+     * @Route("/api/register", name="api_login_check", methods={"POST"})
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+
+        $form = $this->createForm(RegisterType::class, $user, array(
+            'csrf_protection' => false,
+        ));
+
+        $content = $request->getContent();
+        if (!empty($content)) {
+            $data = json_decode($content, true);
+            $form->submit($data);
+        } else {
+            $form->handleRequest($request);
+        }
+
+        if ($form->isValid()) {
+            $user->setPassword($encoder->encodePassword($user, $user->getPassword()));
+            $this->userService->save($user);
+
+            $this->get('session')->getFlashBag()->add(
+                'notice',
+                'User registered !'
+            );
+
+            return new JsonResponse(array(
+                'token' => $this->get('lexik_jwt_authentication.jwt_manager')->create($user)
+            ));
+        }
+
+        return new JsonResponse(array(
+            'message' => $form->getErrors(true)->current()->getMessage(),
+        ), 401);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -3,12 +3,16 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\Transformer\TagTransformer;
+use App\Services\TagService;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SettingsType extends AbstractType
+class RegisterType extends AbstractType
 {
     /**
      * Build Form
@@ -18,7 +22,8 @@ class SettingsType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('apiKey', TextType::class);
+        $builder->add('email', TextType::class);
+        $builder->add('password', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
