fe4369f7f38f342ef8ffc7fc46ba471fb9eacd8b
Mathieu Ledru <matyo91@gmail.com>
2018-09-25 18:21:21 +0200

    alert

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  * @ORM\Table(name="dw_user")
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  * @UniqueEntity("email", message="The email '{{ value }}' is already used.")
- * @UniqueEntity("username", message="The username '{{ value }}' is already used.")
+ * @UniqueEntity("username", message="The username '{{ value }}' is already taken.")
  */
 class User implements UserInterface, \Serializable
 {
