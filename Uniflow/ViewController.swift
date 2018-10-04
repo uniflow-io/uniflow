2ed948c03c289206414ecff691386be385d84ba3
Mathieu Ledru <matyo91@gmail.com>
2018-10-04 17:04:05 +0200

    platform-mac

---
+++ Uniflow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Uniflow
+//
+//  Created by Mathieu Ledru on 04/10/2018.
+//  Copyright © 2018 Mathieu Ledru. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
