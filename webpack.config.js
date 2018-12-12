8454e22df8e155657d74771e30be677e70aeae7d
Mathieu Ledru <matyo91@gmail.com>
2018-12-12 18:04:23 +0100

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ const fs = require('fs')
 
 module.exports = (env) => {
     let buildEnv = ['.env', '.env.local', `.env.${env.NODE_ENV}.local`].reduce((item, envPath) => {
-        if(path.existsSync(path.resolve(__dirname, envPath))) {
+        if(fs.existsSync(path.resolve(__dirname, envPath))) {
             return Object.assign(item, dotenv.parse(fs.readFileSync(path.resolve(__dirname, envPath))));
         }
         return item
-    }, {})
+    }, env)
     buildEnv = Object.keys(buildEnv).reduce((prev, next) => {
         prev[`process.env.${next}`] = JSON.stringify(buildEnv[next]);
         return prev;
