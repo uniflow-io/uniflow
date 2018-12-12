f02360eef8331c9573dacffa2a8f73a1167db34c
Mathieu Ledru <matyo91@gmail.com>
2018-12-12 17:26:27 +0100

    Add github-connect

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App'
 
 const ENV = {
     facebookAppId: process.env.FACEBOOK_APP_ID,
+    githubAppId: process.env.GITHUB_APP_ID,
 }
 
 render(<App env={ENV} />, document.getElementById('app'))
