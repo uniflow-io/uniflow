0296551c2ad9ffd4dd34b124d4522fd67f17b55c
Mathieu Ledru <matyo91@gmail.com>
2018-12-13 17:04:22 +0100

    Update webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,60 @@
-const path = require('path');
+const webpack = require('webpack')
+const dotenv  = require('dotenv')
+const path    = require('path')
+const fs      = require('fs')
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bash.js',
-        publicPath: "/",
-        path: path.resolve(__dirname, '../back/public/dist/js')
-    },
-    devServer: {
-        headers: {
-            "Access-Control-Allow-Origin": "*"
+module.exports = (env) => {
+    let envMode  = env && env.NODE_ENV ? env.NODE_ENV : 'development'
+    let buildEnv = ['.env', '.env.local', `.env.${envMode}.local`].reduce((item, envPath) => {
+        if (fs.existsSync(path.resolve(__dirname, envPath))) {
+            return Object.assign(item, dotenv.parse(fs.readFileSync(path.resolve(__dirname, envPath))));
+        }
+        return item
+    }, env || {})
+    buildEnv     = Object.keys(buildEnv).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(buildEnv[next]);
+        return prev;
+    }, {});
+
+    let loaders = [{
+        loader: 'babel-loader',
+        options: {
+            presets: ['env', 'react', 'es2015', 'flow', 'stage-0']
+        }
+    }]
+    if (envMode !== 'development') {
+        loaders.push({
+            loader: 'cache-loader'
+        })
+    }
+
+    return {
+        entry: './src/index.js',
+        output: {
+            filename: 'bash.js',
+            publicPath: "/",
+            path: path.resolve(__dirname, '../back/public/dist/js')
         },
-        contentBase: "./dist/js",
-        publicPath: "/",
-        filename: "bundle.js",
-        historyApiFallback: true
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env', 'react', 'es2015', 'flow', 'stage-0']
-                }
-            }
-        }]
-    },
-    devtool: 'source-map',
-    target: 'node'
+        devServer: {
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            contentBase: "./dist/js",
+            publicPath: "/",
+            filename: "bash.js",
+            historyApiFallback: true
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                use: loaders
+            }]
+        },
+        plugins: [
+            new webpack.DefinePlugin(buildEnv)
+        ],
+        devtool: envMode === 'development' ? 'source-map' : 'none',
+        target: 'node'
+    }
 };
