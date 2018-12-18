144dda322cbdb8e5a80b7a357ef6dc33b9f6184b
Mathieu Ledru <matyo91@gmail.com>
2018-12-18 16:50:18 +0100

    Update packaging

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import React from 'react'
-import { render } from 'react-dom'
-
 import App from './App'
 
-const ENV = {
-    facebookAppId: process.env.FACEBOOK_APP_ID,
-    githubAppId: process.env.GITHUB_APP_ID,
-}
-
-render(<App env={ENV} />, document.getElementById('app'))
+export { App }
