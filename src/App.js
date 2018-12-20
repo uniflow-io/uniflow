9f5878ddbad699516f8c84871630207d093296c2
Mathieu Ledru <matyo91@gmail.com>
2018-12-20 17:35:26 +0100

    Try import directly

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {Provider, connect} from 'react-redux'
 import routes, {pathTo} from './routes'
 import reducers from './reducers/index'
 import { UserManager } from './components/index'
-import utils from 'uniflow'
+import { createStore } from 'uniflow/src/utils'
 import {getNewLogs,commitReadLog} from './reducers/logs/actions'
 import {commitLoginUserSuccess, commitLogoutUser} from './reducers/auth/actions'
 import {commitSetEnv} from './reducers/env/actions'
@@ -18,8 +18,7 @@ import {withRouter, matchPath} from 'react-router'
 //import createBrowserHistory from 'history/createBrowserHistory'
 
 //const history = createBrowserHistory()
-console.log(utils)
-let store = utils.createStore(reducers)
+let store = createStore(reducers)
 let token = localStorage.getItem('token');
 if (token !== null) {
     store.dispatch(commitLoginUserSuccess(token));
