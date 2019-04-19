e0b71d2aea4c2d9b82f6a17c093463922b009b45
Mathieu Ledru <matyo91@gmail.com>
2019-04-19 18:39:33 +0200

    Update

--- uniflow-front/src/uniflow/index.js
+++ uniflow-front/src/uniflow/index.js
@@ -3,9 +3,3 @@ import ComponentJavascript from 'uniflow-javascript-component/src'
 export default {
     'javascript': ComponentJavascript
 }
-
-/*import ComponentInclude from './components/include'
-
-export default Object.assign(components, {
-  //include: ComponentInclude,
-})*/
