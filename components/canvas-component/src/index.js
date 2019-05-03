6e0235a41b75eda10437fd78c2440043377de2f2
Mathieu Ledru <matyo91@gmail.com>
2019-05-03 16:11:14 +0200

    Update canvas-component

--- components/canvas-component/src/index.js
+++ components/canvas-component/src/index.js
@@ -1,107 +1,140 @@
-import React, { Component } from 'react'
-import { ICheckBoxComponent } from 'uniflow/src/components'
-import { uniflow } from '../package'
-import { onCompile, onExecute } from '../clients/uniflow'
+import React, {Component} from 'react'
+import {ICheckBoxComponent} from 'uniflow/src/components'
+import {uniflow} from '../package'
+import {onCompile, onExecute} from '../clients/uniflow'
 
 export default class CheckBoxesComponent extends Component {
-    state = {
-      running: false,
-      variable: null
+  state = {
+    running: false,
+    variable: null,
+    width: null,
+    height: null
+  }
+
+  static tags() {
+    return uniflow.tags
+  }
+
+  static clients() {
+    return uniflow.clients
+  }
+
+  componentDidMount() {
+    const {bus} = this.props
+
+    bus.on('reset', this.deserialise)
+    bus.on('compile', onCompile.bind(this))
+    bus.on('execute', onExecute.bind(this))
+  }
+
+  componentWillUnmount() {
+    const {bus} = this.props
+
+    bus.off('reset', this.deserialise)
+    bus.off('compile', onCompile.bind(this))
+    bus.off('execute', onExecute.bind(this))
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const oldProps = this.props
+
+    if (nextProps.bus !== oldProps.bus) {
+      oldProps.bus.off('reset', this.deserialise)
+      oldProps.bus.off('compile', onCompile.bind(this))
+      oldProps.bus.off('execute', onExecute.bind(this))
+
+      nextProps.bus.on('reset', this.deserialise)
+      nextProps.bus.on('compile', onCompile.bind(this))
+      nextProps.bus.on('execute', onExecute.bind(this))
     }
+  }
 
-    static tags() {
-        return uniflow.tags
-    }
+  serialise = () => {
+    return [this.state.variable, this.state.width, this.state.height]
+  }
 
-    static clients() {
-        return uniflow.clients
-    }
+  deserialise = data => {
+    let [variable, width, height] = data || [null, null, null]
 
-    componentDidMount () {
-      const { bus } = this.props
+    this.setState({variable: variable, width: width, height: height})
+  }
 
-      bus.on('reset', this.deserialise)
-        bus.on('compile', onCompile.bind(this))
-        bus.on('execute', onExecute.bind(this))
-    }
+  onChangeVariable = event => {
+    this.setState({variable: event.target.value}, this.onUpdate)
+  }
 
-    componentWillUnmount () {
-      const { bus } = this.props
+  onChangeWidth = event => {
+    this.setState({width: event.target.value}, this.onUpdate)
+  }
 
-      bus.off('reset', this.deserialise)
-        bus.off('compile', onCompile.bind(this))
-        bus.off('execute', onExecute.bind(this))
-    }
+  onChangeHeight = event => {
+    this.setState({height: event.target.value}, this.onUpdate)
+  }
 
-    componentWillReceiveProps (nextProps) {
-        const oldProps = this.props
+  onUpdate = () => {
+    this.props.onUpdate(this.serialise())
+  }
 
-        if (nextProps.bus !== oldProps.bus) {
-            oldProps.bus.off('reset', this.deserialise)
-            oldProps.bus.off('compile', onCompile.bind(this))
-            oldProps.bus.off('execute', onExecute.bind(this))
+  onDelete = event => {
+    event.preventDefault()
 
-            nextProps.bus.on('reset', this.deserialise)
-            nextProps.bus.on('compile', onCompile.bind(this))
-            nextProps.bus.on('execute', onExecute.bind(this))
-        }
-    }
+    this.props.onPop()
+  }
 
-    serialise = () => {
-      return [this.state.variable]
-    }
+  render() {
+    const {running, variable, width, height} = this.state
 
-    deserialise = data => {
-      let [variable] = data || [null, {}]
+    return (
+      <div className='box box-info'>
+        <form className='form-horizontal'>
+          <div className='box-header with-border'>
+            <h3 className='box-title'>
+              <button type='submit' className='btn btn-default'>{running ? <i className='fa fa-refresh fa-spin'/> :
+                <i className='fa fa-refresh fa-cog'/>}</button>
+              Checkboxes
+            </h3>
+            <div className='box-tools pull-right'>
+              <button className='btn btn-box-tool' onClick={this.onDelete}><i className='fa fa-times'/></button>
+            </div>
+          </div>
+          <div className='box-body'>
+            <div className='form-group'>
+              <label htmlFor='variable{{ _uid }}' className='col-sm-2 control-label'>Variable</label>
 
-      this.setState({ variable: variable })
-    }
-
-    onChangeVariable = event => {
-      this.setState({ variable: event.target.value }, this.onUpdate)
-    }
-
-    onUpdate = () => {
-      this.props.onUpdate(this.serialise())
-    }
-
-    onDelete = event => {
-      event.preventDefault()
-
-      this.props.onPop()
-    }
-
-    render () {
-      const { running, variable } = this.state
-
-      return (
-        <div className='box box-info'>
-          <form className='form-horizontal'>
-            <div className='box-header with-border'>
-              <h3 className='box-title'><button type='submit' className='btn btn-default'>{running ? <i className='fa fa-refresh fa-spin' /> : <i className='fa fa-refresh fa-cog' />}</button> Checkboxes</h3>
-              <div className='box-tools pull-right'>
-                <button className='btn btn-box-tool' onClick={this.onDelete}><i className='fa fa-times' /></button>
+              <div className='col-sm-10'>
+                <input id='variable{{ _uid }}' type='text' value={variable || ''} onChange={this.onChangeVariable}
+                       className='form-control'/>
               </div>
             </div>
-            <div className='box-body'>
-              <div className='form-group'>
-                <label htmlFor='variable{{ _uid }}' className='col-sm-2 control-label'>Variable</label>
 
-                <div className='col-sm-10'>
-                  <input id='variable{{ _uid }}' type='text' value={variable || ''} onChange={this.onChangeVariable} className='form-control' />
-                </div>
-              </div>
+            <div className='form-group'>
+              <label htmlFor='width{{ _uid }}' className='col-sm-2 control-label'>Width</label>
 
-              <div className='form-group'>
-                <label htmlFor='canvas{{ _uid }}' className='col-sm-2 control-label'>Canvas</label>
-
-                <div className='col-sm-10'>
-                  <canvas ref={(canvas) => this.canvas = canvas} id="canvas{{ _uid }}" width="100%" height="500px"/>
-                </div>
+              <div className='col-sm-10'>
+                <input id='width{{ _uid }}' type='text' value={width || ''} onChange={this.onChangeWidth}
+                       className='form-control'/>
               </div>
             </div>
-          </form>
-        </div>
-      )
-    }
+
+            <div className='form-group'>
+              <label htmlFor='height{{ _uid }}' className='col-sm-2 control-label'>Height</label>
+
+              <div className='col-sm-10'>
+                <input id='height{{ _uid }}' type='text' value={height || ''} onChange={this.onChangeHeight}
+                       className='form-control'/>
+              </div>
+            </div>
+
+            <div className='form-group'>
+              <label htmlFor='canvas{{ _uid }}' className='col-sm-2 control-label'>Canvas</label>
+
+              <div className='col-sm-10'>
+                <canvas ref={(canvas) => this.canvas = canvas} id="canvas{{ _uid }}" width={width} height={height} />
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
