04de783629cb6028cd2c7ee0a08eee07c1bae567
Mathieu Ledru <matyo91@gmail.com>
2019-10-19 22:48:03 +0200

    chrome bridge

---
+++ clients/chrome-client/src/bridges/chrome.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const serializeFunction = (f) => {
+  const serialized = f.toString();
+
+  // Safari serializes async arrow functions with an invalid `function` keyword.
+  // This needs to be removed in order for the function to be interpretable.
+  const safariPrefix = 'async function ';
+  if (serialized.startsWith(safariPrefix)) {
+    const arrowIndex = serialized.indexOf('=>');
+    const bracketIndex = serialized.indexOf('{');
+    if (arrowIndex > -1 && (bracketIndex === -1 || arrowIndex < bracketIndex)) {
+      return `async ${serialized.slice(safariPrefix.length)}`;
+    }
+  }
+
+  return serialized;
+};
+
+export default (background) => {
+  return {
+    httpGet: (url, config, callback) => {
+      return axios.get(url, config).then((response) => {
+        callback(response.data)
+      })
+    },
+    httpPost: (url, data, config, callback) => {
+      return axios.post(url, data, config).then((response) => {
+        callback(response.data)
+      })
+    },
+    evaluateInBackground: (asyncFunction, args) => {
+      asyncFunction = serializeFunction(asyncFunction)
+
+      return Promise.resolve()
+        .then(() => {
+          return background.evaluateInBackground(asyncFunction, args)
+        })
+    },
+    evaluateInContent: (tabId, asyncFunction, args) => {
+      asyncFunction = serializeFunction(asyncFunction)
+
+      return Promise.resolve()
+        .then(() => {
+          return background.evaluateInContent(tabId, asyncFunction, args)
+        })
+    },
+  }
+}
+
--- clients/chrome-client/src/models/Runner.js
+++ clients/chrome-client/src/models/Runner.js
@@ -1,22 +1,24 @@
 import consoleBridge from '../bridges/console'
+import chromeBridge from '../bridges/chrome'
 import vm from 'vm'
 
 export default class Runner {
-    constructor(api, background) {
-        this.api = api
-        this.background = background
-    }
+  constructor(api, background) {
+    this.api = api
+    this.background = background
+  }
 
-    run(rail) {
-        const context = new vm.createContext({
-            console: consoleBridge(this.background)
-        });
+  run(rail) {
+    const context = new vm.createContext({
+      console: consoleBridge(this.background),
+      chrome: chromeBridge(this.background),
+    });
 
-        return rail.reduce((promise, flow) => {
-            return promise.then(() => {
-                return vm.runInContext(flow.code || '', context);
-            })
-        }, Promise.resolve())
-    }
+    return rail.reduce((promise, flow) => {
+      return promise.then(() => {
+        return vm.runInContext(flow.code || '', context);
+      })
+    }, Promise.resolve())
+  }
 }
 
