bc1b2f635eb06aef98b525905b31074729c86cd6
Mathieu Ledru <matyo91@gmail.com>
2019-10-19 14:12:23 +0200

    prompt

--- clients/node-client/src/models/Runner.js
+++ clients/node-client/src/models/Runner.js
@@ -1,5 +1,7 @@
 const bashBridge = require('../bridges/bash')
 const consoleBridge = require('../bridges/console')
+const readlineBridge = require('../bridges/readline')
+const processBridge = require('../bridges/process')
 const vm = require('vm')
 
 function Runner(commandArgs, api) {
@@ -11,6 +13,8 @@ Runner.prototype.run = function (rail) {
   const context = new vm.createContext({
     Bash: bashBridge(this.commandArgs),
     console: consoleBridge,
+    readline: readlineBridge,
+    process: processBridge,
   });
 
   return rail.reduce((promise, flow) => {
