a5065e9d56933ecf2ac185561258bf93fa2944cf
Mathieu LEDRU <matyo91@gmail.com>
2019-12-24 00:37:09 +0100

    textprompt

--- library/jetbrains-client/src/main/java/io/uniflow/bridges/TextPrompt.java
+++ library/jetbrains-client/src/main/java/io/uniflow/bridges/TextPrompt.java
@@ -1,4 +1,13 @@
 package io.uniflow.bridges;
 
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
 public class TextPrompt {
+    private AnActionEvent event;
+
+    public TextPrompt(AnActionEvent event) {
+        this.event = event;
+    }
+
+
 }
