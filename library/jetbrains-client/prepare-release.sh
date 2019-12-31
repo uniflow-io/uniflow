3c5559250f1c5ee64a3a9281594865cf820c7b67
Mathieu LEDRU <matyo91@gmail.com>
2019-12-31 14:06:35 +0100

    changes

--- library/jetbrains-client/prepare-release.sh
+++ library/jetbrains-client/prepare-release.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-echo "<html><ul>" > change-notes.html
-git log `git describe --tags --abbrev=0`..HEAD --no-merges --oneline --pretty=format:"<li>%h %s (%an)</li>" >> change-notes.html
-echo "</ul></html>" >> change-notes.html
+echo "<html>" > change-notes.html
+echo "<h1>Changes for version 1.0.4</h1>" >> change-notes.html
+echo "<ul>" >> change-notes.html
+git log 4b3daf3b..HEAD --no-merges --oneline --pretty=format:"<li>%h %s (%an)</li>" >> change-notes.html
+echo "" >> change-notes.html
+echo "</ul>" >> change-notes.html
+echo "<p>more changes can be found at https://github.com/uniflow-io/uniflow/tree/master/library/jetbrains-client/CHANGELOG.md</p>" >> change-notes.html
+echo "</html>" >> change-notes.html
 
 cp change-notes.html src/main/resources/META-INF/
 
