bd25146758ea8a44c7e4112ef90aeb07bb391678
Mathieu Ledru <matyo91@gmail.com>
2020-03-13 15:30:58 +0100

    :zap: contact

--- library/uniflow-api/src/services/contact.ts
+++ library/uniflow-api/src/services/contact.ts
@@ -37,7 +37,7 @@ export default class ContactService {
 
     <style>
         td a {
-            color:#CCCCCC;
+            color:#007bff;
             text-decoration:none;
         }
         a:hover {
@@ -45,21 +45,20 @@ export default class ContactService {
         }
 
         #content {
-            background-image: url('https://uniflow.io/images/mail/content.jpg');
+            background-image: url('https://api.uniflow.io/images/mail/content.jpg');
             background-repeat: repeat-y no-repeat;
-            background-color: #202020;
+            background-color: #007bff;
             margin: 0;
             padding: 0;
             width: 520px;
         }
-
     </style>
 
 </head>
-<body bgcolor="#000000" topmargin="0" leftmargin="0" marginheight="0" marginwidth="0" style="-webkit-font-smoothing: antialiased;width:100% !important;background:#000000;-webkit-text-size-adjust:none;">
-<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#000000">
+<body bgcolor="#FFFFFF" topmargin="0" leftmargin="0" marginheight="0" marginwidth="0" style="-webkit-font-smoothing: antialiased;width:100% !important;background:#FFFFFF;-webkit-text-size-adjust:none;">
+<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#FFFFFF">
     <tr>
-        <td bgcolor="#000000" width="100%">
+        <td bgcolor="#FFFFFF" width="100%">
 
             <table width="600" cellpadding="0" cellspacing="0" border="0" align="center">
                 <tr>
@@ -67,7 +66,7 @@ export default class ContactService {
                         <table id="content" width="520" cellpadding="0" cellspacing="0" border="0" align="center">
                             <tr>
                                 <td>
-                                    <img src="https://uniflow.io/images/mail/header.jpg" alt="header" width="520" height="104" style="display: block; border: 0;">
+                                    <img src="https://api.uniflow.io/images/mail/header.jpg" alt="header" width="520" height="104" style="display: block; border: 0;">
                                 </td>
                             </tr>
                             <tr>
@@ -77,8 +76,8 @@ export default class ContactService {
                                             <td>
                                                 <table cellpadding="0" cellspacing="0" border="0">
                                                     <tr>
-                                                        <td style="color:#808080;font-size:13px;font-family:Helvetica,Arial,sans-serif; line-height:12px;">
-                                                            <span style="color:#EEEEEE; font-size:20px; font-weight: 300;">Welcome Mathieu !</span><br /><br />
+                                                        <td style="color:#6c757d;font-size:13px;font-family:Helvetica,Arial,sans-serif; line-height:12px;">
+                                                            <span style="color:#007bff; font-size:20px; font-weight: 300;">Welcome Mathieu !</span><br /><br />
                                                         
                                                             A new contact form has been filled on Uniflow :<br /><br />
                                                             email : ${contact.email}<br />
@@ -89,8 +88,8 @@ export default class ContactService {
                                             </td>
                                         </tr>
                                         <tr topmargin="40">
-                                            <td style="color:#808080; font-size:10px; font-family:Helvetica,Arial,sans-serif; line-height:14px; text-align: center;">
-                                                This is an automatic message from <a href="https://uniflow.io">Uniflow</a>.<br />
+                                            <td style="color:#6c757d; font-size:10px; font-family:Helvetica,Arial,sans-serif; line-height:14px; text-align: center;">
+                                                This is an automatic message from <a href="https://api.uniflow.io">Uniflow</a>.<br />
                                                 Do not respond.
                                             </td>
                                         </tr>
