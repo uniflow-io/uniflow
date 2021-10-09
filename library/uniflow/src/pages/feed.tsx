b4240c7e98ec79b19774cdb93b1c0e55c4481708
Mathieu Ledru <matyo91@gmail.com>
2021-10-09 22:24:49 +0200

    :arrow_up: deps

--- library/uniflow/src/pages/feed.tsx
+++ library/uniflow/src/pages/feed.tsx
@@ -1,3 +1,20 @@
+import { graphql } from 'gatsby';
+
 import Page from '@uniflow-io/uniflow-client/src/pages/feed'
+
+export const query = graphql`
+  query {
+    localFlows: allNpmLocalPackage(filter: { fields: { catalogs: { in: "flow" } } }) {
+      nodes {
+        name
+        uniflow {
+          clients
+          name
+          tags
+        }
+      }
+    }
+  }
+`;
 
 export default Page;
--- library/uniflow/src/pages/library.tsx
+++ library/uniflow/src/pages/library.tsx
@@ -1,3 +1,20 @@
+import { graphql } from 'gatsby';
+
 import Page from '@uniflow-io/uniflow-client/src/pages/library'
+
+export const query = graphql`
+  query {
+    localLibrary: allNpmLocalPackage {
+      nodes {
+        name
+        description
+        fields {
+          slug
+          catalogs
+        }
+      }
+    }
+  }
+`;
 
 export default Page;
