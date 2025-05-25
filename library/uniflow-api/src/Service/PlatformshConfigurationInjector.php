63ef49e6896fc47a485b8c987bc75d1a01818406
Mathieu Ledru <matyo91@gmail.com>
2025-05-25 16:34:24 +0200

    :lipstick:

--- library/uniflow-api/src/Service/PlatformshConfigurationInjector.php
+++ library/uniflow-api/src/Service/PlatformshConfigurationInjector.php
@@ -1,8 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Service;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+use function sprintf;
 
 class PlatformshConfigurationInjector
 {
